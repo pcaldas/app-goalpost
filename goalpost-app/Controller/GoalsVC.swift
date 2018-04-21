aaec09661039612b2fdc511474eb60b87e07b47c
Paulo Caldas <paulocaldas@MacBook-Pro-de-Paulo.local>
2018-04-21 00:52:02 -0300

    create the GoalVC and the tableView

--- goalpost-app/Controller/GoalsVC.swift
+++ goalpost-app/Controller/GoalsVC.swift
@@ -9,16 +9,19 @@
 import UIKit
 
 class GoalsVC: UIViewController {
-
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func addGoalBtnWasPressed(_ sender: Any) {
+        tableView.isHidden = false
     }
+    
 
 
 }
