2a5ba6aea4409039b05a78bfdfeb05d206de1dc2
Paulo Caldas <paulocaldas@MacBook-Pro-de-Paulo.local>
2018-04-22 00:55:35 -0300

    start the tableView

---
+++ goalpost-app/Enums/GoalType.swift
@@ -0,0 +1,14 @@
+//
+//  GoalType.swift
+//  goalpost-app
+//
+//  Created by Paulo Caldas on 22/04/2018.
+//  Copyright © 2018 PMC. All rights reserved.
+//
+
+import Foundation
+
+enum GoalType: String {
+    case longTerm = "Long Term"
+    case shortTerm = "Short Term"
+}
